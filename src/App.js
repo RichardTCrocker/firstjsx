7c3ccf0af94d3c495d4c3fb620532cb59c2de089
RichardTCrocker <richardtcrocker@gmail.com>
2023-08-02 14:29:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <p>* Learn React</p>
+      <p>* Climb Mt. Everest</p>
+      <p>* Run a marathon</p>
+      <p>* Feed the cats!</p>
     </div>
   );
 }
